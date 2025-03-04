daaf5ced7a8412da4792bf150ae1dbc6b3f8af1a
root <root@LIYL66-PF51YP6V.lenovo.com>
2025-03-04 14:24:10 +0800

    项目配置定制化和利用go embed进行一体化打包

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yanlong-l/go-mall/config"
 )
 
 func main() {
@@ -11,6 +12,16 @@ func main() {
 	server.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Pong",
+		})
+	})
+	server.GET("config-read", func(ctx *gin.Context) {
+		db := config.Database
+		app := config.App
+		ctx.JSON(http.StatusOK, gin.H{
+			"dsn":         db.DSN,
+			"maxlifetime": db.MaxLifeTime,
+			"app_name":    app.Name,
+			"app_env":     app.Env,
 		})
 	})
 	server.Run(":9000")
