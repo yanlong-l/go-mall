07204fdf348a22de12547e6e5e356345c3471466
yanlong-l <1961445898@qq.com>
2025-03-13 21:31:08 +0800

    定义项目Error记录错误链路和错误发生位置

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"github.com/yanlong-l/go-mall/common/errcode"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +13,7 @@ import (
 
 func main() {
 	server := gin.Default()
-	server.Use(gin.Logger(), middleware.StartTrace())
+	server.Use(middleware.StartTrace(), middleware.LogAccess(), middleware.GinPanicRecovery())
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Pong",
@@ -31,6 +33,23 @@ func main() {
 		logger.Info(ctx, "logger test", "name", "zhangsan", "age", 18)
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "ok",
+		})
+	})
+	server.GET("/customized-error-test", func(ctx *gin.Context) {
+		// 使用 Wrap 包装原因error 生成 项目error
+		err := errors.New("test error-111")
+		aAppErr := errcode.Wrap("包装错误", err)
+		bAppErr := errcode.Wrap("新包装错误", aAppErr)
+		logger.Error(ctx, "打印错误", "err", bAppErr)
+
+		// 预定义的ErrServer, 给其追加错误原因的error
+		err = errors.New("a domain error")
+		apiErr := errcode.ErrServer.WithCause(err)
+		logger.Error(ctx, "API执行中出现错误", "err", apiErr)
+
+		ctx.JSON(apiErr.HttpStatusCode(), gin.H{
+			"code": apiErr.Code,
+			"msg":  apiErr.Msg,
 		})
 	})
 	server.Run(":9000") // listen and serve on 0.0.0.0:9000
